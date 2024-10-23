3b61fa7a14cbf498231b3edc7b83c0065b45fca3
Gunars Frickauss <gunarsf@gmail.com>
2024-10-23 11:49:27 +0300

    Update chhost.sh

--- chhost.sh
+++ chhost.sh
@@ -1,13 +1,10 @@
 !/bin/bash
 set -e
-
 CURRENT_HOSTNAME=$(cat /proc/sys/kernel/hostname)
 IPO=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1 |  cut -d. -f2);
 NEW_HOSTNAME="rapi-ib11-"$IPO
-
 echo "Changing hostname from ${CURRENT_HOSTNAME} ${NEW_HOSTNAME}..."
 #Update hostname file
 printf "%s\n" "${NEW_HOSTNAME}" | sudo tee /etc/hostname >/dev/null
-
 #Update hosts
 sudo sed -i "s/127.0.1.1.*/127.0.1.1\t${NEW_HOSTNAME}/g" /etc/hosts
