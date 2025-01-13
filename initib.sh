21e11fe2edcdc08bdf804e068c7c0cf89026b9b0
Gunars Frickauss <gunarsf@gmail.com>
2025-01-13 13:16:08 +0200

    Update initib.sh

--- initib.sh
+++ initib.sh
@@ -2,7 +2,18 @@
 
 raspi-config --expand-rootfs > /dev/null
 set -e
-/opt/ib/set_ib.sh
+HOME_DIR='/opt/ib'
+${HOME_DIR}/set_ib.sh
+#Get media &news
+"${HOME_DIR}/sync_media.sh"
+"${HOME_DIR}/sync_news.sh"
+#Set crontab enteries
+printf "Setting crontab enteries\n locally\n"
+"${HOME_DIR}/set_cronjobs.sh"
+ printf "Setting hostname...\n"
+${HOME_DIR}/set_hostname.sh"
+sleep 2
+
 systemctl enable ib.service
 systemctl disable firstboot.service
 
